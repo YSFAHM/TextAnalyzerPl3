16f4670bb570931dfc08dfaedc101a3e820dfc59
yassin <ym9807770@gmail.com>
2024-12-09 22:46:00 +0200

    ConstructLayout

--- TextAnalyzerPl3/Gui.fs
+++ TextAnalyzerPl3/Gui.fs
@@ -21,3 +21,39 @@ let createStyledButton (text: string) (width: int) (backColor: Color) =
     button.Region <- new Region(new Drawing.RectangleF(0.0f, 0.0f, button.Width |> float32, button.Height |> float32))
 
     button
+let createTextAnalyzerApp () =
+    let form = new Form(Text = "Text Analyzer", Width = 400, Height = 500, StartPosition = FormStartPosition.CenterScreen)
+
+    
+    
+    form.FormBorderStyle <- FormBorderStyle.FixedDialog
+    form.MaximizeBox <- false
+    form.MinimizeBox <- false
+    form.BackColor <- Color.White
+
+    
+    form.ClientSize <- new Size(370, 280)
+
+    
+    let buttonPanel = new Panel(Dock = DockStyle.Top, Height = 350)
+    form.Controls.Add(buttonPanel)
+
+    
+    let flowLayout = new FlowLayoutPanel(Dock = DockStyle.Fill, FlowDirection = FlowDirection.LeftToRight, Padding = new Padding(25))
+    buttonPanel.Controls.Add(flowLayout)
+
+    
+    let analyzeButton = createStyledButton "Analyze" 150 Color.LightGreen
+    let loadButton = createStyledButton "Load File" 150 Color.LightBlue
+    let clearButton = createStyledButton "Clear Text" 150 Color.Orange
+    let saveButton = createStyledButton "Save Results" 150 Color.LightSalmon
+    let showResultsButton = createStyledButton "Show Results" 150 Color.LightCoral
+    let showTextButton = createStyledButton "Show Text" 150 Color.LightYellow
+
+    
+    flowLayout.Controls.Add(analyzeButton)
+    flowLayout.Controls.Add(loadButton)
+    flowLayout.Controls.Add(clearButton)
+    flowLayout.Controls.Add(saveButton)
+    flowLayout.Controls.Add(showResultsButton)
+    flowLayout.Controls.Add(showTextButton)
