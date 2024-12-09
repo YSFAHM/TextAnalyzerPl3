d8153d541b78ee1128c0a118aa95cb0a8597a046
YoussefMain <yoyo1236744@gmail.com>
2024-12-09 22:49:04 +0200

    Calling app function

--- TextAnalyzerPl3/Program.fs
+++ TextAnalyzerPl3/Program.fs
@@ -1,2 +1,8 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open Gui
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =
+    createTextAnalyzerApp ()
+    0
