39ab3303c9257e5ab201fd9c872ebaf2e8137a2f
YoussefLawendy <youssefhanyelshahat@gmail.com>
2024-12-11 00:17:06 +0200

    Edit File Structure

--- TextAnalyzerPl3/Gui.fs
+++ TextAnalyzerPl3/Gui.fs
@@ -6,24 +6,6 @@ open System.Windows.Forms
 open System.Drawing
 open Analysis
 
-
-let createStyledButton (text: string) (width: int) (backColor: Color) =
-    let button = new Button(Text = text, Width = width, Height = 50, BackColor = backColor, FlatStyle = FlatStyle.Standard)
-    button.Font <- new Font("Arial", 12.0f, FontStyle.Bold)
-    button.Padding <- new Padding(15)
-    button.ForeColor <- Color.Black 
-    button.BackColor <- backColor
-    button.FlatAppearance.BorderSize <- 1
-    button.FlatAppearance.BorderColor <- Color.Black
-    button.TextAlign <- ContentAlignment.MiddleCenter
-
-    button.MouseEnter.Add(fun _ -> button.BackColor <- Color.LightGray)
-    button.MouseLeave.Add(fun _ -> button.BackColor <- backColor)
-
-    button.Region <- new Region(new Drawing.RectangleF(0.0f, 0.0f, button.Width |> float32, button.Height |> float32))
-
-    button
-
 let showAnalysisResults text =
     let wordCount = countWords text
     let sentenceCount = countSentences text
@@ -49,6 +31,23 @@ Word Frequency:
                                 """ wordCount sentenceCount paragraphCount avgSentenceLength tableRows
 
     analysisResult
+
+let createStyledButton (text: string) (width: int) (backColor: Color) =
+    let button = new Button(Text = text, Width = width, Height = 50, BackColor = backColor, FlatStyle = FlatStyle.Standard)
+    button.Font <- new Font("Arial", 12.0f, FontStyle.Bold)
+    button.Padding <- new Padding(15)
+    button.ForeColor <- Color.Black 
+    button.BackColor <- backColor
+    button.FlatAppearance.BorderSize <- 1
+    button.FlatAppearance.BorderColor <- Color.Black
+    button.TextAlign <- ContentAlignment.MiddleCenter
+
+    button.MouseEnter.Add(fun _ -> button.BackColor <- Color.LightGray)
+    button.MouseLeave.Add(fun _ -> button.BackColor <- backColor)
+
+    button.Region <- new Region(new Drawing.RectangleF(0.0f, 0.0f, button.Width |> float32, button.Height |> float32))
+
+    button
 
 let createTextAnalyzerApp () =
     let form = new Form(Text = "Text Analyzer", Width = 400, Height = 500, StartPosition = FormStartPosition.CenterScreen)
