649e39dd84af2eba375b6c95507161469c6147aa
YoussefLawendy <youssefhanyelshahat@gmail.com>
2024-12-11 00:09:55 +0200

    Construct Analysis Massege

--- TextAnalyzerPl3/Gui.fs
+++ TextAnalyzerPl3/Gui.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open System.Windows.Forms
 open System.Drawing
+open Analysis
+
 
 let createStyledButton (text: string) (width: int) (backColor: Color) =
     let button = new Button(Text = text, Width = width, Height = 50, BackColor = backColor, FlatStyle = FlatStyle.Standard)
@@ -21,6 +23,33 @@ let createStyledButton (text: string) (width: int) (backColor: Color) =
     button.Region <- new Region(new Drawing.RectangleF(0.0f, 0.0f, button.Width |> float32, button.Height |> float32))
 
     button
+
+let showAnalysisResults text =
+    let wordCount = countWords text
+    let sentenceCount = countSentences text
+    let paragraphCount = countParagraphs text
+    let avgSentenceLength = averageSentenceLength text
+    let wordFreq = wordFrequency text
+
+    let tableRows = 
+        wordFreq
+        |> Array.map (fun (word, freq) -> sprintf "word : %s  freq : %d " word freq)
+        |> String.concat "\n"
+
+    let analysisResult = sprintf """
+| Metric                  | Value    |
+|-------------------------|----------|
+| Words                   | %d       |
+| Sentences               | %d       |
+| Paragraphs              | %d       |
+| Average Sentence Length | %.2f     |
+
+Word Frequency:
+%s
+                                """ wordCount sentenceCount paragraphCount avgSentenceLength tableRows
+
+    analysisResult
+
 let createTextAnalyzerApp () =
     let form = new Form(Text = "Text Analyzer", Width = 400, Height = 500, StartPosition = FormStartPosition.CenterScreen)
 
