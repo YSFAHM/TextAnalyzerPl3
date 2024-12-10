2a9048a6806f25a3e748f39881dca1de4539831a
yousefel-naggar <yousef.a.naggar@gmail.com>
2024-12-10 02:03:57 +0200

    complete analysis module

--- TextAnalyzerPl3/Analysis.fs
+++ TextAnalyzerPl3/Analysis.fs
@@ -13,3 +13,14 @@ let countSentences (text: string) =
 let countParagraphs (text: string) = 
     text.Split([|'\n'; '\r'|], StringSplitOptions.RemoveEmptyEntries)
     |> Seq.length
+
+let wordFrequency (text: string) = 
+    text.Split([|' '; '\n'; '\t'; '.'; ','; '!'|], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun word -> word.ToLower())
+    |> Array.countBy id
+    |> Array.sortByDescending snd
+
+let averageSentenceLength text =
+    let totalWords = countWords text
+    let totalSentences = countSentences text
+    if totalSentences > 0 then float totalWords / float totalSentences else 0.0
